38b3065af3a695866d90fe939f0850e0808d43b9
Michael Milewski <saramic@gmail.com>
2019-12-30 17:59:07 +1100

    basic timer and netlify connection
    https://hang-time.netlify.com/

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useInterval } from "./Util";
+import "./App.css";
 
 function App() {
+  const [running, setRunning] = useState(false);
+  const [start, setStart] = useState();
+  const [delta, setDelta] = useState(0);
+  const [timings, setTimings] = useState([]);
+
+  const startTimer = () => {
+    setRunning(true);
+    setStart(new Date().getTime());
+  };
+
+  const stopTimer = () => {
+    setRunning(false);
+    setTimings([...timings, delta]);
+  };
+
+  useInterval(() => {
+    if (running) setDelta((new Date().getTime() - start) / 1000);
+  }, 50);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      timer
+      <div>
+        <span>{delta}</span>
+      </div>
+      <button onClick={startTimer}>start</button>
+      <button onClick={stopTimer}>stop</button>
+      <ul>
+        {timings.reverse().map(timing => (
+          <li key={timing}>{timing}</li>
+        ))}
+      </ul>
     </div>
   );
 }
